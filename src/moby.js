205698ce6ac5a9a1f76a61d74e08598dba172218
= <=>
2024-08-20 05:59:54 -0700

    mv moby.nodes, .graph -> nodes.json, graph.json

--- src/moby.js
+++ src/moby.js
@@ -138,8 +138,8 @@ function read_file_data(filename) {
 
 function load_moby_graph() {
 
-	var nodes_string = read_file_data('moby.nodes');
-	var graph_string = read_file_data('moby.graph');
+	var nodes_string = read_file_data('nodes.json');
+	var graph_string = read_file_data('graph.json');
 
 	var nodes = JSON.parse(nodes_string);
 	var graph = JSON.parse(graph_string);
