24bdd14ee9aa19469a03e7f058ffaad0c7ccf611
Adriaan Canter <adriaan.canter@gmail.com>
2024-08-23 17:32:19 -0700

    Works on RasPi 5 Ubuntu 20.04 FF.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/moby/",
+  server: {
+    host:"136.27.55.214",
+    port:8080
+  },
   plugins: [react()],
 })
