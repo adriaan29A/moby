1e8fe1be578d48fcffbef6d2688b31a0ece90cc2
Adriaan Canter <adriaan.canter@gmail.com>
2024-09-06 07:53:05 -0700

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/moby/",
-	 server: {
-    host:"136.27.55.214",
-    port:8080
-  },
+	base: "/moby",
   plugins: [react()]
 })
